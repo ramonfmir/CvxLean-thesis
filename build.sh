9eac1a6badd1eb5eb56ccf8fdeb494b186a81596
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-03-01 15:44:48 +0000

    chore: have `build.sh` take care of everything

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Build this project.
 
+lake clean
 lake update
 lake exe cache get
 lake build 
@@ -10,9 +11,19 @@ lake build
 
 cd .lake/packages/CvxLean
 
-lake update
 lake exe cache get
 lake run EggClean
 lake build EggPreDCP
 lake build CvxLean
 lake build CvxLeanTest
+
+cd ../../..
+
+# Copy egg-pre-dcp.
+
+cp -r .lake/packages/CvxLean/egg-pre-dcp .
+
+# Re-build the code in this repository, with `pre_dcp` working.
+
+rm -rf .lake/build 
+lake build
