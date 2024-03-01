5971c6f62f90cefff4b66a339b573de355327c0c
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-03-01 15:45:09 +0000

    feat: all code for Chapter 5

--- Thesis/Chapter5.lean
+++ Thesis/Chapter5.lean
@@ -6,7 +6,58 @@ open CvxLean Minimization Real
 
 namespace Chapter5
 
--- HERE
+namespace MotivatingExample
+
+/-! Section 5.1 -/
+
+def p :=
+  optimization (x : ℝ)
+    minimize x
+    subject to
+      c₁ : 1e-3 ≤ x
+      c₂ : 1 / sqrt x ≤ exp x
+
+equivalence eqv₁/p₁ : p := by
+  -- DCP fails with the following error:
+  --    Failure in constraint c₂:
+  --    [Trying atom fun x => rexp x for expression rexp x: Expected concave, but atom is convex]
+  fail_if_success dcp
+
+def qByHand :=
+  optimization (x : ℝ)
+    minimize x
+    subject to
+      c₁ : 1e-3 ≤ x
+      c₂ : exp (-x) ≤ sqrt x
+
+equivalence eqv₂/q₁ : qByHand := by
+  -- Here, it succeeds.
+  dcp
+
+end MotivatingExample
+
+namespace PreDCPProcedure
+
+/-! Section 5.6 -/
+
+open MotivatingExample
+
+-- Now, we show how to get to `q` with the `pre_dcp` tactic.
+
+-- Click on `equivalence*` to see what we send to the `egg` sub-process and what we receive.
+equivalence* eqv/q : p := by
+  pre_dcp
+
+-- NOTE: This is not exactly the same problem we show in the thesis, but it is equivalent and in
+-- DCP form.
+#print q
+#check eqv
+
+solve q
+
+#eval eqv.backward_map q.solution -- 0.426303
+
+end PreDCPProcedure
 
 end Chapter5
 
