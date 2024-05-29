e6f61806273c5d0e4b7f9663759894925316f2f3
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-05-29 14:37:46 +0100

    chore: fix CvxLean commit

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 
 package Thesis
 
-require CvxLean from git "https://github.com/verified-optimization/CvxLean" @ "main"
+require CvxLean from git "https://github.com/verified-optimization/CvxLean" @ "32443811fe7184559f784c9aedce48a4bd2ff3ca"
 
 @[default_target]
 lean_lib Thesis
